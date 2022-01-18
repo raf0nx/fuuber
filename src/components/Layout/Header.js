aa6111663514b2027397267fe7cd0bafd979c3e1
raf0nx <rafalpawlowski2011@gmail.com>
2022-01-18 23:04:30 +0100

    feat: Implement add to cart button

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,12 +1,13 @@
 import styles from "./Header.module.css";
 import mealsImage from "../../assets/meals.jpeg";
+import HeaderCardButton from "./HeaderCardButton";
 
 const Header = props => {
   return (
     <>
       <header className={styles.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCardButton />
       </header>
       <div className={styles["main-image"]}>
         <img src={mealsImage} alt="A table full of delicious meals!" />
