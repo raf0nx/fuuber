981755ef3bb894ea29e29714d99c70ce3cc6d058
raf0nx <rafalpawlowski2011@gmail.com>
2022-01-18 23:54:19 +0100

    feat: Implement single meal component, add Input to UI

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,3 +1,5 @@
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 import styles from "./AvailableMeals.module.css";
 
 const DUMMY_MEALS = [
@@ -28,10 +30,20 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map(meal => <li key={meal.id}>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map(meal => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
   return (
     <section className={styles.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
