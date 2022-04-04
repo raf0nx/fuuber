2a2f4b75da014b350f68c4103ec59ec655cce855
raf0nx <rafalpawlowski2011@gmail.com>
2022-04-05 00:03:10 +0200

    feat: Add Button UI Component

---
+++ src/components/UI/Button/Button.tsx
@@ -0,0 +1,45 @@
+interface ButtonProps {
+  text: string
+  category: 'primary' | 'secondary' | 'error'
+  type?: 'button' | 'submit' | 'reset'
+  name?: string
+  disabled?: boolean
+  classes?: string
+  clickHandler?: () => void
+}
+
+const Button: React.FC<ButtonProps> = ({
+  text,
+  category,
+  type = 'button',
+  name = '',
+  disabled = false,
+  classes = '',
+  clickHandler,
+}) => {
+  let styles =
+    'text-white bg-indigo-500 hover:bg-indigo-600 focus:ring-indigo-300'
+
+  if (category === 'secondary') {
+    styles =
+      'text-gray-900 bg-white border border-gray-200 hover:bg-gray-100 focus:z-10 focus:ring-gray-200'
+  }
+
+  if (category === 'error') {
+    styles = 'text-white bg-red-600 hover:bg-red-700 focus:ring-red-300'
+  }
+
+  return (
+    <button
+      className={`py-2.5 px-5 text-sm font-medium rounded-lg transition-colors duration-200 focus:outline-none focus:ring-2 ${styles} ${classes}`}
+      type={type}
+      name={name}
+      disabled={disabled}
+      onClick={clickHandler}
+    >
+      {text}
+    </button>
+  )
+}
+
+export default Button
