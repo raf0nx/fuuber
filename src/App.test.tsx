a9a11be1511d0144e281e605af801f60a16b30db
raf0nx <rafalpawlowski2011@gmail.com>
2022-04-05 00:17:09 +0200

    refactor: Move App and Auth tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom'
 
-import App from '../App'
+import App from './App'
 
 test('<App /> should render successfully', () => {
   render(
--- src/pages/Auth/Auth.test.tsx
+++ src/pages/Auth/Auth.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 
-import Auth from '../pages/Auth/Auth'
+import Auth from './Auth'
 
 describe('<Auth />', () => {
   test('should change auth mode', () => {
