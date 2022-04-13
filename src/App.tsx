769bd092c8d5c3ac784cfc3774c7d9278816db99
raf0nx <rafalpawlowski2011@gmail.com>
2022-04-13 23:58:00 +0200

    feat: Implement Route protection

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { useAppSelector } from './hooks/store-hooks'
 
 import Auth from './pages/Auth/Auth'
 import Home from './pages/Home/Home'
 
 const App: React.FC = () => {
+  const isLoggedIn = useAppSelector(state => state.auth.isLoggedIn)
+
   return (
     <main id="app" className="h-screen w-screen">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/auth" element={<Auth />} />
+        <Route
+          path="/"
+          element={isLoggedIn ? <Home /> : <Navigate to="/auth" />}
+        />
+        <Route
+          path="/auth"
+          element={isLoggedIn ? <Navigate to="/" /> : <Auth />}
+        />
       </Routes>
     </main>
   )
