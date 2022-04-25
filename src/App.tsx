c6360443858700cccfab4201586826dc71da81be
raf0nx <rafalpawlowski2011@gmail.com>
2022-04-26 00:47:40 +0200

    feat: Persist auth state after page reload

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
+import { useEffect } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
-import { useAppSelector } from './hooks/store-hooks'
 
 import Auth from './pages/Auth/Auth'
 import Home from './pages/Home/Home'
 
+import { useLazyGetUserDataQuery } from './api/auth'
+import { useAppDispatch, useAppSelector } from './hooks/store-hooks'
+import { setTokenData } from './store/auth'
+
 const App: React.FC = () => {
   const isLoggedIn = useAppSelector(state => state.auth.isLoggedIn)
+  const dispatch = useAppDispatch()
+  const [triggerGetUserData] = useLazyGetUserDataQuery()
+
+  useEffect(() => {
+    const idToken = sessionStorage.getItem('idToken')
+    const refreshToken = sessionStorage.getItem('refreshToken')
+    const expiresIn = sessionStorage.getItem('expiresIn')
+
+    const hasAuthData = idToken && refreshToken && expiresIn
+
+    idToken && triggerGetUserData(idToken)
+
+    if (!isLoggedIn && hasAuthData) {
+      dispatch(setTokenData({ idToken, refreshToken, expiresIn }))
+    }
+  }, [dispatch, isLoggedIn, triggerGetUserData])
 
   return (
     <main id="app" className="h-screen w-screen">
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,24 +1,15 @@
-import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 import Button from '../../components/UI/Button/Button'
+
 import { useAppDispatch, useAppSelector } from '../../hooks/store-hooks'
 import { logout } from '../../store/auth'
 
-import { useLazyGetUserDataQuery } from '../../api/auth'
-
 const Home: React.FC = () => {
-  const { idToken, user } = useAppSelector(state => state.auth)
+  const { user } = useAppSelector(state => state.auth)
   const dispatch = useAppDispatch()
 
-  const [triggerGetUserData] = useLazyGetUserDataQuery()
-
   const navigate = useNavigate()
-
-  useEffect(() => {
-    idToken && user?.incompleteData && triggerGetUserData(idToken)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [idToken])
 
   const logoutHandler = () => {
     dispatch(logout())
