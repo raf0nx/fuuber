34100c10a7c2df90762b8761707213aff0974d4e
raf0nx <rafalpawlowski2011@gmail.com>
2022-05-10 23:49:57 +0200

    feat: Make an alert dismissable

--- src/components/UI/Alert/Alert.tsx
+++ src/components/UI/Alert/Alert.tsx
@@ -1,17 +1,22 @@
 import classNames from 'classnames'
 
 import { ReactComponent as InfoIcon } from '../../../assets/icons/info.svg'
+import { ReactComponent as CloseIcon } from '../../../assets/icons/close.svg'
 
 interface AlertProps {
   message: string
   type: 'success' | 'info' | 'warning' | 'danger'
   classes?: string
+  dismissable?: boolean
+  onDismissAlert?: () => void
 }
 
 export const Alert: React.FC<AlertProps> = ({
   message,
   type,
   classes = '',
+  dismissable = false,
+  onDismissAlert,
 }) => {
   const alertStyles = classNames(
     'flex p-4 mb-4 text-sm rounded-lg',
@@ -22,12 +27,43 @@ export const Alert: React.FC<AlertProps> = ({
     classes
   )
 
+  const closeBtnStyles = classNames(
+    'ml-auto -mx-1.5 -my-1.5 rounded-lg focus:ring-2 p-1.5 inline-flex h-8 w-8 focus:outline-none',
+    {
+      'bg-green-100 text-green-500 focus:ring-green-400 hover:bg-green-200':
+        type === 'success',
+    },
+    {
+      'bg-blue-100 text-blue-500 focus:ring-blue-400 hover:bg-blue-200':
+        type === 'info',
+    },
+    {
+      'bg-yellow-100 text-yellow-500 focus:ring-yellow-400 hover:bg-yellow-200':
+        type === 'warning',
+    },
+    {
+      'bg-red-100 text-red-500 focus:ring-red-400 hover:bg-red-200':
+        type === 'danger',
+    }
+  )
+
   return (
     <div className={alertStyles} role="alert">
       <div className="inline flex-shrink-0 mr-3 w-5 h-5">
         <InfoIcon />
       </div>
       <div className="font-medium">{message}</div>
+      {dismissable && (
+        <button
+          onClick={onDismissAlert}
+          type="button"
+          className={closeBtnStyles}
+          aria-label="Close"
+        >
+          <span className="sr-only">Close</span>
+          <CloseIcon />
+        </button>
+      )}
     </div>
   )
 }
