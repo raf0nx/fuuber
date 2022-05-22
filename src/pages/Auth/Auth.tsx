a26ee61c2a7c3fc812375a70e99c76351d355d6e
raf0nx <rafalpawlowski2011@gmail.com>
2022-05-22 21:03:26 +0200

    feat: New auth background

--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -5,12 +5,12 @@ import authBackground from '../../assets/auth-background.webp'
 export const Auth: React.FC = () => {
   return (
     <section className="w-full h-full flex justify-evenly items-center">
-      <img
-        className="absolute -z-10 bg-center object-cover h-full w-full"
-        src={authBackground}
-        alt="Food nicely presented"
+      <div
+        className="absolute bg-repeat h-full w-full -z-10"
+        style={{
+          backgroundImage: `url(${authBackground})`,
+        }}
       />
-      <div />
       <AuthForm />
     </section>
   )
