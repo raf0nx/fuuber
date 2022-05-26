0cbf723c74603c41975c0de5d42b920a971f8817
raf0nx <rafalpawlowski2011@gmail.com>
2022-05-26 23:20:52 +0200

    feat: Add dropdown component

---
+++ src/components/UI/Dropdown/Dropdown.tsx
@@ -0,0 +1,22 @@
+import classNames from 'classnames'
+
+interface DropdownProps {
+  children: JSX.Element | JSX.Element[]
+  classes?: string
+}
+
+export const Dropdown: React.FC<DropdownProps> = ({
+  children,
+  classes = '',
+}) => {
+  return (
+    <div
+      className={classNames(
+        'z-10 absolute bg-white divide-y divide-gray-100 rounded shadow text-gray-700',
+        classes
+      )}
+    >
+      {children}
+    </div>
+  )
+}
