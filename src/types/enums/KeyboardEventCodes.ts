5081ee836f04c33ed1f6ba3a14bb3d3a818a4620
raf0nx <rafalpawlowski2011@gmail.com>
2022-05-27 15:03:17 +0200

    feat: Add enum with keyboard event codes

---
+++ src/types/enums/KeyboardEventCodes.ts
@@ -0,0 +1,9 @@
+export enum KeyboardEventCodes {
+  SPACE = 'Space',
+  ENTER = 'Enter',
+  ESCAPE = 'Escape',
+  ARROW_UP = 'ArrowUp',
+  ARROW_DOWN = 'ArrowDown',
+  ARROW_LEFT = 'ArrowLeft',
+  ARROW_RIGHT = 'ArrowRight',
+}
