3fd50ede4dbb69d1632daa65c94593801a130378
raf0nx <rafalpawlowski2011@gmail.com>
2022-05-27 17:13:30 +0200

    refactor: Clean up Home Screen

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,28 +1,12 @@
-import { useHistory } from 'react-router-dom'
-
-import Button from 'components/UI/Button'
-
-import { useAppDispatch, useAppSelector } from 'hooks/store-hooks'
-import { logout } from 'store/slices/auth'
+import { useAppSelector } from 'hooks/store-hooks'
 
 export const Home: React.FC = () => {
   const { user } = useAppSelector(state => state.auth)
-  const dispatch = useAppDispatch()
-
-  const history = useHistory()
-
-  const logoutHandler = () => {
-    dispatch(logout())
-    history.replace('/auth')
-  }
 
   return (
     <section>
       <div>Welcome home!</div>
       <div>{user?.displayName}</div>
-      <Button category="primary" onClick={logoutHandler}>
-        Logout
-      </Button>
     </section>
   )
 }
