c001283060b9f55e8d3775cae97a83ead539ba21
raf0nx <rafalpawlowski2011@gmail.com>
2022-05-30 01:29:23 +0200

    refactor: Move dropdown opening and accessibility logic to Dropdown component

--- src/components/UI/Dropdown/Dropdown.tsx
+++ src/components/UI/Dropdown/Dropdown.tsx
@@ -1,22 +1,79 @@
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 import classNames from 'classnames'
 
+import { DropdownItem } from './DropdownItem'
+
+import { useElementVisible } from 'hooks/use-element-visible'
+import { isKeyEnterOrSpace } from 'utils/utils'
+
+import { KeyboardEventCodes } from 'types/enums/KeyboardEventCodes'
+
 interface DropdownProps {
-  children: JSX.Element | JSX.Element[]
+  items: JSX.Element[]
   classes?: string
 }
 
 export const Dropdown: React.FC<DropdownProps> = ({
+  items,
   children,
   classes = '',
 }) => {
+  const { ref, isElementVisible, setIsElementVisible } =
+    useElementVisible<HTMLDivElement>(false)
+
+  const location = useLocation()
+
+  useEffect(() => {
+    isElementVisible && setIsElementVisible(false)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location])
+
+  const keyDownHandler = (code: string) => {
+    if (code === KeyboardEventCodes.ESCAPE && isElementVisible) {
+      setIsElementVisible(false)
+      ref.current.focus()
+
+      return
+    }
+
+    if (!isKeyEnterOrSpace(code)) return
+    setIsElementVisible(!isElementVisible)
+  }
+
   return (
-    <ul
-      className={classNames(
-        'z-10 absolute bg-white divide-y divide-gray-100 rounded shadow text-gray-700',
-        classes
-      )}
+    <div
+      onClick={() => setIsElementVisible(!isElementVisible)}
+      onKeyDown={({ nativeEvent }) => keyDownHandler(nativeEvent.code)}
+      className="relative"
+      ref={ref}
+      tabIndex={0}
+      role="button"
+      aria-expanded={isElementVisible}
+      aria-controls={isElementVisible ? 'dropdown-menu' : undefined}
+      aria-haspopup="true"
     >
       {children}
-    </ul>
+      {isElementVisible && (
+        <ul
+          className={classNames(
+            'z-10 absolute bg-white divide-y divide-gray-100 rounded shadow text-gray-700',
+            classes
+          )}
+        >
+          {!!items.length &&
+            items.map((item, index) => (
+              <DropdownItem
+                onBlur={() =>
+                  index === items.length - 1 && setIsElementVisible(false)
+                }
+                key={item.key}
+              >
+                {item}
+              </DropdownItem>
+            ))}
+        </ul>
+      )}
+    </div>
   )
 }
