59494665aa29b5d0193743161fb07758011025b6
raf0nx <rafalpawlowski2011@gmail.com>
2022-05-30 01:27:30 +0200

    feat: Add onBlur prop on DropdownItem

--- src/components/UI/Dropdown/DropdownItem.tsx
+++ src/components/UI/Dropdown/DropdownItem.tsx
@@ -2,14 +2,19 @@ import classNames from 'classnames'
 
 interface DropdownItemProps {
   classes?: string
+  onBlur?: () => void
 }
 
 export const DropdownItem: React.FC<DropdownItemProps> = ({
   children,
   classes = '',
+  onBlur,
 }) => {
   return (
-    <li className={classNames('flex flex-col text-sm py-1', classes)}>
+    <li
+      className={classNames('flex flex-col text-sm py-1', classes)}
+      onBlur={onBlur}
+    >
       {children}
     </li>
   )
