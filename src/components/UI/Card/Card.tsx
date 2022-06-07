ed2e41365836f201f7940b6dcd5072caa20ecfa0
raf0nx <rafalpawlowski2011@gmail.com>
2022-06-08 00:24:28 +0200

    refactor: Refactor Card to be more reusable

--- src/components/UI/Card/Card.tsx
+++ src/components/UI/Card/Card.tsx
@@ -1,29 +1,27 @@
+import classNames from 'classnames'
+
 interface CardProps {
-  title: string
   classes?: string
-  subtitle?: string
-  actions?: JSX.Element
-  actionsClasses?: string
+  tabIndex?: number
+  ariaLabelledby?: string
 }
 
 export const Card: React.FC<CardProps> = ({
   children,
-  title,
-  classes = '',
-  subtitle,
-  actions,
-  actionsClasses = '',
+  classes,
+  tabIndex,
+  ariaLabelledby,
 }) => {
   return (
-    <div
-      className={`bg-white p-6 rounded-lg border border-gray-200 shadow-md ${classes}`}
+    <article
+      className={classNames(
+        'bg-white rounded-lg border border-gray-200 shadow-md',
+        classes
+      )}
+      tabIndex={tabIndex}
+      aria-labelledby={ariaLabelledby}
     >
-      <h1 className="text-2xl antialiased font-bold text-gray-900 mb-2">
-        {title}
-      </h1>
-      {subtitle && <h2 className="text-sm opacity-60">{subtitle}</h2>}
-      {children && <div className="my-8">{children}</div>}
-      {actions && <div className={actionsClasses}>{actions}</div>}
-    </div>
+      {children}
+    </article>
   )
 }
