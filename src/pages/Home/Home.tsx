88104cb4749b59d7ac16c91a2c9716e52fd4e84f
raf0nx <rafalpawlowski2011@gmail.com>
2022-06-13 23:25:01 +0200

    feat: Add no food found state

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,9 +3,11 @@ import FoodCardItem from 'components/FoodCardItem'
 import { useAppSelector } from 'hooks/store-hooks'
 import { useGetFoodsQuery } from 'api/food'
 
+import { ReactComponent as NoFoodIcon } from 'assets/icons/no-food.svg'
+
 export const Home: React.FC = () => {
   const { user } = useAppSelector(state => state.auth)
-  const { data: foods } = useGetFoodsQuery()
+  const { data: foods, isSuccess } = useGetFoodsQuery()
 
   return (
     <section className="container mx-auto p-4">
@@ -22,11 +24,22 @@ export const Home: React.FC = () => {
       <h2 className="text-xl font-semibold text-gray-900 mb-8">
         Available meals ({foods?.length})
       </h2>
-      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 auto-rows-auto gap-8">
-        {foods?.map(item => (
-          <FoodCardItem item={item} key={item.id} />
-        ))}
-      </div>
+      {!!foods?.length && isSuccess && (
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 auto-rows-auto gap-8">
+          {foods.map(item => (
+            <FoodCardItem item={item} key={item.id} />
+          ))}
+        </div>
+      )}
+      {!foods?.length && isSuccess && (
+        <div className="flex flex-col items-center gap-4">
+          <NoFoodIcon />
+          <h2 className="text-xl text-center font-semibold text-gray-900 mt-4">
+            No meals available at this moment.
+          </h2>
+          <p className="text-slate-500">We have to get our chefs to work...</p>
+        </div>
+      )}
     </section>
   )
 }
