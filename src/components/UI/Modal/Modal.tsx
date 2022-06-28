9bf979e97195cc2cd9518d8eac93854bb513c193
raf0nx <rafalpawlowski2011@gmail.com>
2022-06-28 23:53:32 +0200

    feat: Add Modal component

---
+++ src/components/UI/Modal/Modal.tsx
@@ -0,0 +1,29 @@
+import ReactDOM from 'react-dom'
+import { CSSTransition } from 'react-transition-group'
+
+import Backdrop from '../Backdrop'
+
+export const Modal: React.FC<{ show: boolean }> = ({ show, children }) => (
+  <>
+    <Backdrop show={show} />
+    {ReactDOM.createPortal(
+      <CSSTransition
+        in={show}
+        timeout={{
+          enter: 300,
+          exit: 500,
+        }}
+        classNames={{
+          enterActive: 'animate-slide-in',
+          exitActive: 'animate-slide-out',
+        }}
+        unmountOnExit
+      >
+        <div className="absolute z-10 top-0 left-0 w-full h-full flex items-center justify-center">
+          {children}
+        </div>
+      </CSSTransition>,
+      document.body
+    )}
+  </>
+)
